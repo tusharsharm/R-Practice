46ceb910508a6ac11ce857944a086f8e4bc5d93e
Tushar Sharma <124043637+tusharsharm@users.noreply.github.com>
2025-02-01 13:34:46 +0530

    Update R charts and graphs.R

--- R charts and graphs.R
+++ R charts and graphs.R
@@ -9,16 +9,29 @@ percentages <- round(100 * values / sum(values), 1)
 labels_with_percent <- paste(labels, percentages, "%")
 pie(values, labels = labels_with_percent, main = "Pie Chart with Percentages", col = rainbow(length(values)))
 
+values2 <- c(15, 25, 35, 25)
+labels2 <- c("W", "X", "Y", "Z")
+pie(values2, labels = labels2, main = "Pie Chart Example 2", col = rainbow(length(values2)))
+
+
 # Bar plot example
 barplot(values, names.arg = labels, main = "Bar Plot Example", col = "skyblue", xlab = "Categories", ylab = "Values")
 
 # Box plot example
 boxplot(values, main = "Box Plot Example", ylab = "Values", col = "blue")
 
+values3 <- c(5, 15, 25, 35)
+labels3 <- c("M", "N", "O", "P")
+barplot(values3, names.arg = labels3, main = "Bar Plot Example 2", col = "pink", xlab = "Categories", ylab = "Values")
+
 # Scatter plot example
 x <- c(1, 2, 3, 4, 5, 8, 9.5, 2.6, 8, 8.5)
 y <- c(2, 4, 6, 8, 10, 5.5, 6.6, 4.4, 9.9, 4.7)
 plot(x, y, main = "Scatter Plot Example", xlab = "X-axis", ylab = "Y-axis", col = rainbow(length(values)), pch = 16)
+
+x2 <- c(2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+y2 <- c(1, 3, 5, 7, 9, 11, 13, 15, 17, 19)
+plot(x2, y2, main = "Scatter Plot Example 2", xlab = "X-axis", ylab = "Y-axis", col = "purple", pch = 18)
 
 # Calculating mean and median
 mean(x, trim = 0, na.rm = FALSE)
